ea677833147786bf4d7699ff39fecc537ac550b6
invisiblegeek1 <appumoorthy15@gmail.com>
2021-06-22 16:15:14 +0530

    some bug fixed

--- src/com/example/os/ResourceAllocator.java
+++ src/com/example/os/ResourceAllocator.java
@@ -2,9 +2,13 @@ package com.example.os;
 import  com.example.os.Requestor;
 import com.example.os.RamAllocator;
 public class ResourceAllocator implements Requestor,RamAllocator {
+    // RAM value has initialized
+    public int RAM =20;
+
+    // resource checker..
     private void resourceChecker(String requestorName, int requestedResource){
-        int RAM=20;
-        if(requestedResource<=RAM){
+
+        if(requestedResource<=this.RAM){
             System.out.println(requestorName +" requesting " +requestedResource+ " resources....");
             ramManager(requestorName,requestedResource);
         }
@@ -14,17 +18,22 @@ public class ResourceAllocator implements Requestor,RamAllocator {
 
 
     }
+
+    //It overrides request method a
     @Override
     public void request(String requestorName, int requestedResource) {
         resourceChecker(requestorName,requestedResource);
 
 
     }
+
+    // RAM manager method has implemented
     @Override
     public void ramManager(String appName,int ramCount){
         System.out.println(ramCount + " GB allocated for "+appName+"....");
     }
 
+    // main method
     public static void main(String[] args){
         ResourceAllocator a1= new ResourceAllocator();
         a1.request("PUBG",10);
